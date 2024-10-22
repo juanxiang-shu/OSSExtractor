5eb3a5a4f5258ec1b7f3e9ee0e351b100969d518
Juan Xiang <139843957+juanxiang-shu@users.noreply.github.com>
2024-10-22 16:18:53 +0800

    Update Abstract.py

--- Text Extraction/Abstract.py
+++ Text Extraction/Abstract.py
@@ -2,10 +2,10 @@ from gpt4all import GPT4All
 import pandas as pd
 import os
 import re
-def Model_1_5(df):
+def Model_2(df):
     abstract = []
     for index, row in df.iterrows():
-        content = row            ['content']
+        content = row['content']
 
         prompt_template_for_abstract_text = (
             f"{content}"
@@ -13,8 +13,7 @@ def Model_1_5(df):
             f"Please summarize the text below, emphasizing the types of reactions featured in the author's scientific experiments on surface reactions."            
         )
         model = GPT4All('nous-hermes-llama2-13b.Q4_0.gguf', allow_download=False)
-        abstract_text = model.generate(prompt=prompt_template_for_abstract_text, max_tokens=250,  temp=0.0,top_k=25,
-                                       top_p=0.6,                                  min_p=0.2, repeat_penalty=3.7,  repeat_last_n=0, n_batch =1)
+        abstract_text = model.generate(prompt=prompt_template_for_abstract_text, max_tokens=250, temp=0.0, top_p=0.6)
         print("abstract_text:")
         print(abstract_text)
         abstract.append(abstract_text)
